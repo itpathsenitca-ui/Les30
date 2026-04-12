e67f557149b2c376cd83a9b0c7161a6d60be71c0
itpathsenitca-ui <itpathsenitca@gmail.com>
2026-04-12 14:21:46 +0300

    fix: new update

--- test1.py
+++ test1.py
@@ -5,7 +5,6 @@ for i in range(100):
     print(f"save{i}.jpg")
 print("data set is ready")
 print("hello")
-<<<<<<< HEAD
 print("1")
 print("2")
 print("3")
@@ -24,7 +23,6 @@ print("15")
 print("16")
 print("17")
 print("18")
-=======
 print("hello and godbay and also my English is the best in the world")
->>>>>>> 11e00db7868e329f28541bf8d37a36fb55b21ebb
 print("super idol ble ble ble")
+print("fr")
--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
 print("War Thunder is downloading")
-for i in range(100):
+for i in range(101):
     print(f"progress:{i}%")
 print("War Thunder Downloaded")
