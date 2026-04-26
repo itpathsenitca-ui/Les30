0ccc06bd51ba33745867cb5353572ccc7c7bf3cb
itpathsenitca-ui <itpathsenitca@gmail.com>
2026-04-26 12:39:32 +0300

    fix:1515115151151511515115151151511515115151151511515115151151511515115151151511515115151151511515115151151511515115151151511515115151

--- task1.py
+++ task1.py
@@ -133,3 +133,14 @@ while True:
     cv2.waitKey(
         1
     )
+    print(1)
+    print(2)
+    print(3)
+    print(4)
+    print(5)
+    print(6)
+    print(7)
+    print(8)
+    print(9)
+    print(10)
+    print(11)
