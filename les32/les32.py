6f77a1e89323d19d51be80a9e8cc176fa401c371
itpathsenitca-ui <itpathsenitca@gmail.com>
2026-05-03 13:06:58 +0300

    fix: a few fixes

--- les32/les32.py
+++ les32/les32.py
@@ -1,17 +1,5 @@
 import cv2
-cv2.namedWindow('setting')
-def noth(x):
-    pass
-cv2.createTrackbar("Hmin","setting",0,255,noth)
-cv2.createTrackbar("Smin","setting",0,255,noth)
-cv2.createTrackbar("Vmin","setting",0,255,noth)
-cv2.createTrackbar("Hmax","setting",0,255,noth)
-cv2.createTrackbar("Smax","setting",0,255,noth)
-cv2.createTrackbar("Vmax","setting",0,255,noth)
-def range():
-    return  (cv2.getTrackbarPos("Hmin","setting"),
-                cv2.getTrackbarPos("Smin","setting"),
-                cv2.getTrackbarPos("Vmin", "setting"),)
-(cv2.getTrackbarPos("Hmax","setting"),
-                cv2.getTrackbarPos("Smax","setting"),
-                cv2.getTrackbarPos("Vmax", "setting"),)
+from utils.createSetting import range
+while True:
+        img1 = cv2.imread("./img/photo_5447115728428405764_y.jpg")
+        img2 = cv2.imread("./img/photo_5447115728428405765_y.jpg")
